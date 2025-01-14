b6e0aa2d824b9adfbe79e6e72818ac12c85691cb
lorevlmt <lore.vimontes@gmail.com>
2025-01-14 14:13:54 +0100

    Ejercicio 25 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,15 +4,27 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
+        Stack<String> stack = new Stack<>();
 
+        stack.push("Smith");
+        stack.push("Montessori");
+        stack.push("Peralta");
+        stack.push("House");
+
+        return stack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
 
+        Stack<String> stack = createStack();
+
+        printAndEmptyStack(stack);
     }
 
 
